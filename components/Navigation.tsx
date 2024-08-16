15fc4e39ef0deb2d3e0f8e8fe50bf537a30e1987
FRANK022530 <franklin.delosreyes@cvsu.edu.ph>
2024-08-16 13:05:02 +0800

    Initial Commit

---
+++ components/Navigation.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='p-5 flex justify-center lg:justify-end lg:mr-8'>
+        <ul className='flex'>
+            <li className='p-4'>About Us</li>
+            <li className='p-4'>Products</li>
+            <li className='p-4'>Reviews</li>
+        </ul>
+    
+    </div>
+  )
+}
+
+export default Navigation

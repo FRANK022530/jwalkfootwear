03217921de5da212139f6b019afc201f438427ee
FRANK022530 <franklin.delosreyes@cvsu.edu.ph>
2024-08-17 14:58:39 +0800

    Modfying UI for Responsiveness

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Reviews = () => {
   return (
-    <div className="carousel w-10/12 bg-blue-100">
-  <div id="slide1" className="carousel-item relative w-10/12 ">
- <div className='w-full bg-red-500 h-64 flex justify-center items-center'>
-    <div className='w-10/12 h-5/6 bg-white'>
-        Review
+    <div className='flex justify-center '>
+    <div className="carousel w-full ">
+  <div id="slide1" className="carousel-item relative w-full ">
+ <div className='w-full  h-64 flex justify-center items-center'>
+    <div className='w-10/12 h-5/6 bg-white rounded-lg border-2'>
+        Review1
     </div>
  </div>
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -14,10 +15,10 @@ const Reviews = () => {
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide2" className="carousel-item relative w-10/12">
-  <div className='w-full bg-yellow-500 h-64 flex justify-center items-center'>
-    <div className='w-10/12 h-5/6 bg-white'>
-        Review
+  <div id="slide2" className="carousel-item relative w-full">
+  <div className='w-full  h-64 flex justify-center items-center'>
+    <div className='w-10/12 h-5/6 bg-white rounded-lg border-2'>
+        Review2
     </div>
  </div>
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -25,21 +26,21 @@ const Reviews = () => {
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide3" className="carousel-item relative w-10/12">
-  <div className='w-full bg-green-500 h-64 flex justify-center items-center'>
-    <div className='w-10/12 h-5/6 bg-white'>
-        Review
+  <div id="slide3" className="carousel-item relative w-full">
+  <div className='w-full  h-64 flex justify-center items-center'>
+    <div className='w-10/12 h-5/6 bg-white rounded-lg border-2'>
+        Review3
     </div>
  </div>
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide4" className="btn btn-circle">❯</a>
+      <a href="#slide4" className="btn btn-circle ">❯</a>
     </div>
   </div>
-  <div id="slide4" className="carousel-item relative w-10/12">
-  <div className='w-full bg-blue-500 h-64 flex justify-center items-center'>
-    <div className='w-10/12 h-5/6 bg-white'>
-        Review
+  <div id="slide4" className="carousel-item relative w-full">
+  <div className='w-full  h-64 flex justify-center items-center'>
+    <div className='w-10/12 h-5/6 bg-white rounded-lg border-2'>
+        Review4
     </div>
  </div>
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -47,6 +48,7 @@ const Reviews = () => {
       <a href="#slide1" className="btn btn-circle">❯</a>
     </div>
   </div>
+</div>
 </div>
   )
 }
